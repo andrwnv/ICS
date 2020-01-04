82ed249f813dbd4f27095e51eed41621d15b5cf2
andrwvn <glazynovand@gmail.com>
2020-01-05 03:23:39 +0700

    fix index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const electron      = require('electron');
-const browserWinow  = electron.BrowserWinowdow;
+const browserWindow  = electron.BrowserWindow;
 const app           = electron.app;
 
 const path          = require('path');
@@ -7,7 +7,7 @@ const path          = require('path');
 let mainWindow = null;
 
 const createWindow = () => {
-    mainWindow = new browserWinow( { width: 1280, height: 720 } );
+    mainWindow = new browserWindow( { width: 1280, height: 720 } );
     mainWindow.loadURL( require('url').format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',

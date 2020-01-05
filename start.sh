117f1709a5c8458fbb7db3352d4f4bd929134a60
andrwvn <glazynovand@gmail.com>
2020-01-05 19:50:23 +0700

    update start script

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
+echo "install npm modules"
+npm install > /dev/null
+
+if [[ -z $(command -v zerorpc) ]]; then
+    echo "install zerorpc"
+    pip3 install zerorpc > /dev/null 
+fi
+
+echo "---------- ICS START ----------"
 python3 ICS_Server/serverAPI.py &
 npm start &

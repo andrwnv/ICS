3d22ddf6ff6eda53bfb787f31e643896e9977046
Andrew G <42163705+andrwnv@users.noreply.github.com>
2020-01-19 01:11:02 +0700

    Update RpcNetworking.py

--- ICS_Server/app/network/RpcNetworking.py
+++ ICS_Server/app/network/RpcNetworking.py
@@ -7,7 +7,7 @@ from zerorpc import Server
 
 class RpcNetworking(IRpcNetworking):
     """
-        Class to connect, disconect and recieve info from the server.
+        Class to connect, disconect and recieve info.
     """
 
     def __init__(self):

965c0d803b26b3258c9320d1486561c3468805f7
Глазунов Андрей Юрьевич <aglazunov@tomsk.lemz.ru>
2020-01-19 19:09:14 +0700

    add global controller

--- app/js/global_controller.js
+++ app/js/global_controller.js
@@ -1,6 +1,6 @@
 const rpc = require('zerorpc');
 
-let client = new rpc.Client();
+var client = new rpc.Client();
 client.connect('tcp://127.0.0.1:4242');
 
-export {client};
+module.exports.client = client;

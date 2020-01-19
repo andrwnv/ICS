0e7641218a05c4ac42e65d68479d53e3733ede10
Глазунов Андрей Юрьевич <aglazunov@tomsk.lemz.ru>
2020-01-20 00:04:24 +0700

    go to prototype class

--- app/js/authorization_controller.js
+++ app/js/authorization_controller.js
@@ -1,13 +1,15 @@
-const { RPCNetworking } = require('/home/andrew/Desktop/ICS/app/js/global_controller');
+let RPCNetworking = require('./js/rpc_networking');
 
-globalController.client.invoke('echo', 'server ready', (error, res, more) => {
+let network = new RPCNetworking();
+
+network.invoke('echo', 'server ready', (error, res, more) => {
     console.log(res);
 });
 
 let btn = document.querySelector('#login_button');
 
 btn.addEventListener('click', () => {
-    globalController.client.invoke('echo', 'do_something', (error, res, more) => {
+    network.invoke('echo', 'do_something', (error, res, more) => {
         console.log(res);
     });
 });
